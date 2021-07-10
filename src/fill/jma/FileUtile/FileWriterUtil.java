daa77a50f6d4e2bfae4285870cd2d7b392622a51
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2021-07-10 17:03:23 +0300

    Read and Write file

--- src/fill/jma/FileUtile/FileWriterUtil.java
+++ src/fill/jma/FileUtile/FileWriterUtil.java
@@ -1,5 +1,60 @@
 package fill.jma.FileUtile;
 
-public class FileWriterUtil {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class FileWriterUtil implements Runnable {
+
+    Thread t;
+    String name;
+
+    FileWriterUtil(String threadName) {
+        name = threadName;
+        t = new Thread(this, name);
+        System.out.println("New thread" + " " + t);
+        t.start();
+    }
+
+    @Override
+    public synchronized void run() {
+
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        int average = 0;
+        for (int j = 0; j < 10; j++) {
+            list.add(j, random.nextInt(100));
+            average += list.get(j) / 10;
+        }
+        appendInt(list, "/Users/user/Desktop/ReadFile/src/fill/jma/FileUtile/DemoText");
+        System.out.println(list + "   average this thread is: " + average);
+    }
+
+    public static void allThread() throws InterruptedException {
+        new FileWriterUtil("tOne");
+        new FileWriterUtil("tTwo");
+        new FileWriterUtil("tThree");
+        new FileWriterUtil("tFour");
+        new FileWriterUtil("tFive");
+        new FileWriterUtil("tSix");
+        new FileWriterUtil("tSeven");
+        new FileWriterUtil("tEight");
+        new FileWriterUtil("tNine");
+        new FileWriterUtil("tTen");
+    }
+
+    public static void appendInt(List<Integer> list, String path) {
+        File file = new File(path);
+        try (FileWriter fileWriter = new FileWriter(file, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(String.valueOf(list));
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Ups, failed to write into file: " + path, e);
+        }
+    }
 }
--- src/fill/jma/Main.java
+++ src/fill/jma/Main.java
@@ -1,12 +1,16 @@
 package fill.jma;
 
 import fill.jma.FileUtile.FileReaderUtil;
+import fill.jma.FileUtile.FileWriterUtil;
 
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(FileReaderUtil.readFile("data/input.txt"));
+    public static void main(String[] args) throws InterruptedException, IOException {
+        FileWriterUtil.allThread();
+        System.out.println(FileReaderUtil.readFile("/Users/user/Desktop/ReadFile/src/fill/jma/FileUtile/DemoText"));
+
     }
 }
